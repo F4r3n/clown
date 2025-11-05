229e62e66790520a01119528df5a8191ae73fc3e
F4r3n <guillaume_k@hotmail.fr>
2025-11-05 12:48:51 +0000

    more bench

--- clown/benches/message_parser.rs
+++ clown/benches/message_parser.rs
@@ -1,4 +1,4 @@
-use clown::irc_view::message_parser::{is_string_plain, to_spans};
+use clown::irc_view::message_parser::{get_size_without_format, is_string_plain, to_spans};
 use criterion::{Criterion, criterion_group, criterion_main};
 use std::hint::black_box;
 
@@ -9,6 +9,11 @@ fn bench_create_spans(message: &str) -> anyhow::Result<()> {
 
 fn bench_is_message_plain(message: &str) -> anyhow::Result<()> {
     is_string_plain(message);
+    Ok(())
+}
+
+fn bench_get_size_witout_format(message: &str) -> anyhow::Result<()> {
+    get_size_without_format(message);
     Ok(())
 }
 
@@ -28,7 +33,30 @@ fn criterion_benchmark(c: &mut Criterion) {
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
             ))
-        })
+        });
+        b.iter(|| {
+            bench_create_spans(black_box(
+                "Lorem Ipsum is simply \x038,4Hi! text of the printing and \x038,4Hi! industry.
+            Lorem Ipsum has \x038,4Hi! the industry's standard \x038,4Hi! text ever since the 1500s",
+            ))
+        });
+
+    });
+
+    c.bench_function("get_size_witout_format", |b| {
+        b.iter(|| {
+            bench_get_size_witout_format(black_box(
+                "Lorem Ipsum is simply \x038,4Hi! text of the printing and \x038,4Hi! industry.
+            Lorem Ipsum has \x038,4Hi! the industry's standard \x038,4Hi! text ever since the 1500s",
+            ))
+        });
+
+        b.iter(|| {
+            bench_get_size_witout_format(black_box(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            ))
+        });
     });
 }
 
